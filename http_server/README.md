a8aba32afe2adfa39b6d2411e285fe3f1d15161c
Cotbert2 <mateojgdev@gmail.com>
2025-07-30 09:09:49 -0500

    http server

---
+++ http_server/README.md
@@ -0,0 +1,14 @@
+# Web Server
+This is a web server built in Node js
+It aims just to be a simple example of how to create a web server using Node.js and Express.
+
+
+# RUN
+
+Just run the following command in the terminal:
+```bash
+sudo npm start
+```
+
+Please ensure you have **Node.js** and **npm** installed on your system.
+
---
+++ http_server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 80;
+
+// Servir archivos estáticos desde la carpeta public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
